a078003e6cfa3f4482f38a3068bb4e1fd2adc8fd
Rodolfo Mariano de Souza <rodolfo.mariano@outlook.com>
2021-04-08 16:32:28 -0300

    Created the base of project

---
+++ src/styles/theme.ts
@@ -0,0 +1,23 @@
+import { extendTheme } from "@chakra-ui/react"
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      "50": "#F5F8FA",
+      "400": "#999999",
+      "600": "#47585B",
+    }
+  },
+  fonts: {
+    heading: 'Poppins',
+    body: 'Poppins',
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.50',
+        color: 'gray.600',
+      }
+    }
+  }
+})
